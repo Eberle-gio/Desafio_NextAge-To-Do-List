93484e7b921981c96cbf772ab344aab99807e472
Eberle-gio <giovanalopes@gmail.com>
2025-08-05 10:54:47 -0300

    :sparkles: Adds create, remove, and complete task functionality, plus component styling improvements

--- frontEnd/to-do-list-project/src/App.jsx
+++ frontEnd/to-do-list-project/src/App.jsx
@@ -25,15 +25,48 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) => {
+    const newTodo = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 1000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodo);
+  };
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteresTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteresTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todos todo={todo} />
+          <Todos
+            key={todo.id}
+            todo={todo}
+            removeTodo={removeTodo}
+            completeTodo={completeTodo}
+          />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
--- frontEnd/to-do-list-project/src/components/Todos.jsx
+++ frontEnd/to-do-list-project/src/components/Todos.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-export const Todos = ({ todo }) => {
+export const Todos = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div
+      className="todo"
+      style={{
+        textDecoration: todo.isCompleted ? "line-through" : "",
+        backgroundColor: todo.isCompleted ? "#ecececff" : "#fff",
+      }}
+    >
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="completed">Completar</button>
-        <button className="remove">excluir</button>
+        <button className="completed" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          excluir
+        </button>
       </div>
     </div>
   );
